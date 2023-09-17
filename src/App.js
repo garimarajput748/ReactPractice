35474379cae0a635f12fa26e4bb5634985a90573
Garima Rajput <garimarajput748@gmail.com>
2023-09-17 15:26:49 +0530

    deploy site on github

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-import State from './component/State';
-import Carousal from './component/Carousal';
-import Faq from './component/Faq';
+// import State from './component/State';
+// import Carousal from './component/Carousal';
+// import Faq from './component/Faq';
 import APIData from './component/Effect';
-import UseEffectAPI from './component/effectApi/AsyncAwaitApi';
+// import UseEffectAPI from './component/effectApi/AsyncAwaitApi';
 
 function App() {
   return (
